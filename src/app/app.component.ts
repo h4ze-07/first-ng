c0044569b539a4222f8db0c35691fe400da3fc17
Vladyslav <122152653+h4ze-07@users.noreply.github.com>
2024-09-04 13:45:33 +0300

    updated...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,33 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './common-ui/header/header.component';
 import { FetchDataService } from './data/services/fetch-data.service';
 import { UsersI } from './data/interfaces';
+import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, HeaderComponent, ProfileCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ng-proj';
   usersService = inject(FetchDataService);
+
+  randomData = [
+    {
+        id: '01',
+        name: 'John',
+    },
+    {
+        id: '02',
+        name: 'Mark',
+    },
+    {
+        id: '03',
+        name: 'Alex',
+    },
+  ]
 
   usersData: UsersI | null = null;
 
